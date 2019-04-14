2fd89ece67d05039b8ecd676bcdbb613521eaa96
Bryant Finney <bryant@outdoorlinkinc.com>
2019-04-14 02:10:31 -0500

    lots of updates

--- pymint/arith.py
+++ pymint/arith.py
@@ -1,3 +1,14 @@
+"""A dummy placeholder module for performing arithmmetic operations.
+
+.. module:: arith
+   :synopsis: a dummy placeholder module
+
+.. moduleauthor:: Bryant Finney <bryant@outdoorlinkinc.com>
+   :github: https://github.com/bryant-finney
+"""
+
+
 def add(x, y):
-    """Add two numbers"""
+    """Add two numbers."""
+
     return x + y
--- pymint/tests/test_arith.py
+++ pymint/tests/test_arith.py
@@ -1,5 +1,17 @@
-from package_template import add
+"""
+Dummy test script for the dummy template module.
+
+.. module:: pymin.tests.test_arith
+   :synopsis: test the dummy template module
+
+.. moduleauthor:: Bryant Finney <bryant@outdoorlinkinc.com>
+   :github: https://github.com/bryant-finney
+"""
+
+from pymint.arith import add
+
 
 def test_add():
+    """Make sure it can add."""
     assert add(2, 2) == 4
     assert add(-2, 7) == 5
